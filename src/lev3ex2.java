9859924152ee2944e28582c37ce9db3af7b8fd0e
SspidesS <99414766+SspidersS@users.noreply.github.com>
2022-06-14 17:15:26 +0200

    .

--- src/lev3ex2.java
+++ src/lev3ex2.java
@@ -24,4 +24,16 @@
 2
  */
 
+import java.util.Scanner;
 
+public class lev3ex2 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (number > 0 && number !=0){
+            System.out.println(number );
+        }
+
+    }
+}
