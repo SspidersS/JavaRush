6cb2e2eca4b9c94ab6fe4bbaeae1ed507450ab1c
SspidesS <99414766+SspidersS@users.noreply.github.com>
2022-06-14 16:47:22 +0200

    .

--- src/Lev3Lec9Age18.java
+++ src/Lev3Lec9Age18.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 public class Lev3Lec9Age18 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-         String name = scanner.nextLine();
-         int age = scanner.nextInt();
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
 
 //        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
 //         String name = buff.readLine();
 //         int age = buff.read();
 
-         if (age > 20) {
-             System.out.println("И 18-ти достаточно");
-         } if (age <= 20){
-         }
+        if (age > 20) {
+            System.out.println("И 18-ти достаточно");
+        }
+        if (age <= 20) {
+        }
     }
 }
 
---
+++ src/lev3.java
@@ -0,0 +1,39 @@
+
+/*
+Введи с клавиатуры целое число в диапазоне 1 - 999. Выведи его строку-описание следующего вида:
+
+        «четное однозначное число» - если число четное и имеет одну цифру;
+        «нечетное однозначное число» - если число нечетное и имеет одну цифру;
+        «четное двузначное число» - если число четное и имеет две цифры;
+        «нечетное двузначное число» - если число нечетное и имеет две цифры;
+        «четное трехзначное число» - если число четное и имеет три цифры;
+        «нечетное трехзначное число» - если число нечетное и имеет три цифры.
+        Если введенное число не попадает в диапазон 1 - 999, не нужно ничего выводить на экран.
+        Пример для числа 100:
+        четное трехзначное число
+
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class lev3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if (number > 0 && number < 10 && number % 2 == 0) {
+            System.out.println("четное однозначное число");
+        } else if (number > 0 && number < 10 && number % 2 != 0) {
+            System.out.println("нечетное однозначное число");
+        } else if (number > 9 && number < 100 && number % 2 == 0) {
+            System.out.println("четное двузначное число");
+        } else if (number > 9 && number < 100 && number % 2 != 0) {
+            System.out.println("нечетное двузначное число");
+        } else if (number > 99 && number < 1000 && number % 2 == 0) {
+            System.out.println("четное трехзначное число");
+        } else if (number > 99 && number < 1000 && number % 2 != 0) {
+            System.out.println("нечетное трехзначное число");
+        }
+    }
+}
